0bae21f7fbab8ccc9c53e366abd12cb84b9c01b8
wleal797 <leal.wilson19@gmail.com>
2025-02-12 12:31:12 -0500

    1st commit
    Proyecto java Electrodomésticos Adonai desarrollo de software

---
+++ Software_ElectroAdonai/src/main/java/co/edu/unbosque/software_electroadonai/SoftwareElectroAdonaiApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.software_electroadonai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareElectroAdonaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareElectroAdonaiApplication.class, args);
+	}
+
+}
