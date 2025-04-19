7c9811cd05d170b53253df864e719e1e913e3c1a
wleal797 <leal.wilson19@gmail.com>
2025-04-18 19:19:17 -0500

    editar y eliminar

--- Software_ElectroAdonai/src/main/java/co/edu/unbosque/software_electroadonai/controller/ProductoController.java
+++ Software_ElectroAdonai/src/main/java/co/edu/unbosque/software_electroadonai/controller/ProductoController.java
@@ -1,14 +1,17 @@
 package co.edu.unbosque.software_electroadonai.controller;
 
+import co.edu.unbosque.software_electroadonai.model.Empleado;
 import co.edu.unbosque.software_electroadonai.model.Producto;
 import co.edu.unbosque.software_electroadonai.services.ProductoDAO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/productos")
@@ -42,4 +45,33 @@ public class ProductoController {
         System.out.println(productos.toString());
         return "lista-productos";
     }
+
+    @PostMapping("/editar")
+    public String editarProducto(@ModelAttribute Producto nuevoProducto) {
+        Optional<Producto> productoExistente = productoDAO.getProductoById(nuevoProducto.getID_PRODUCTO());
+
+        if (productoExistente.isPresent()) {
+            Producto producto = productoExistente.get();
+            producto.setNOMBRE_PRODUCTO(nuevoProducto.getNOMBRE_PRODUCTO());
+            producto.setREFERENCIA(nuevoProducto.getNOMBRE_PRODUCTO());
+            producto.setCOSTO(nuevoProducto.getCOSTO());
+            producto.setEXISTENCIAS(nuevoProducto.getEXISTENCIAS());
+
+            productoDAO.saveOrUpdate(producto);
+        }
+        return "redirect:/productos/listar";
+    }
+
+
+    @DeleteMapping("/eliminar/{id}")
+    public ResponseEntity<String> eliminarProducto(@PathVariable int id) {
+        Optional<Producto> productoExistente = productoDAO.getProductoById(id);
+        if (productoExistente.isPresent()) {
+            productoDAO.deleteProducto(id);
+            return ResponseEntity.ok("Producto eliminado correctamente");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Producto no encontrado");
+        }
+    }
+
 }
