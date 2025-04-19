fe9690552d98489c770a828c9c2a724b6d247a03
wleal797 <leal.wilson19@gmail.com>
2025-04-18 19:19:17 -0500

    editar y eliminar

--- Software_ElectroAdonai/src/main/java/co/edu/unbosque/software_electroadonai/controller/EmpleadoController.java
+++ Software_ElectroAdonai/src/main/java/co/edu/unbosque/software_electroadonai/controller/EmpleadoController.java
@@ -3,6 +3,8 @@ package co.edu.unbosque.software_electroadonai.controller;
 import co.edu.unbosque.software_electroadonai.model.Empleado;
 import co.edu.unbosque.software_electroadonai.services.EmpleadoDAO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +12,11 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
+import java.util.Optional;
+
 
 @Controller
 @RequestMapping("/empleados")
@@ -18,7 +24,6 @@ public class EmpleadoController {
 
     @Autowired
     private EmpleadoDAO empleadoDAO;
-
     @GetMapping("/")
 
     public String inicio() {
@@ -42,7 +47,39 @@ public class EmpleadoController {
         model.addAttribute("empleados", empleados);
         return "lista-empleados";
     }
+    @PostMapping("/editar")
+    public String editarEmpleado(@ModelAttribute Empleado nuevoEmpleado) {
+        Optional<Empleado> empleadoExistente = empleadoDAO.getEmpleadoById(nuevoEmpleado.getID_EMPLEADO());
+
+        if (empleadoExistente.isPresent()) {
+            Empleado empleado = empleadoExistente.get();
+            empleado.setN_DOCUMENTO(nuevoEmpleado.getN_DOCUMENTO());
+            empleado.setNOMBRE_EMPLEADO(nuevoEmpleado.getNOMBRE_EMPLEADO());
+            empleado.setTELEFONO_EMPLEADO(nuevoEmpleado.getTELEFONO_EMPLEADO());
+
+            empleadoDAO.saveOrUpdate(empleado);
+        }
+        return "redirect:/empleados/listar";
+    }
+
+
+    @DeleteMapping("/eliminar/{id}")
+    public ResponseEntity<String> eliminarEmpleado(@PathVariable int id) {
+        Optional<Empleado> empleadoExistente = empleadoDAO.getEmpleadoById(id);
+        if (empleadoExistente.isPresent()) {
+            empleadoDAO.deleteEmpleado(id);
+            return ResponseEntity.ok("Empleado eliminado correctamente");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Empleado no encontrado");
+        }
+    }
+
 }
+
+
+
+
+
 
 
 
--- Software_ElectroAdonai/src/main/java/co/edu/unbosque/software_electroadonai/controller/UsuarioController.java
+++ Software_ElectroAdonai/src/main/java/co/edu/unbosque/software_electroadonai/controller/UsuarioController.java
@@ -1,58 +1,74 @@
 package co.edu.unbosque.software_electroadonai.controller;
 
-import co.edu.unbosque.software_electroadonai.model.Authorities;
-import co.edu.unbosque.software_electroadonai.model.Users;
-import co.edu.unbosque.software_electroadonai.services.AutoridadesDAO;
+import co.edu.unbosque.software_electroadonai.model.Empleado;
+import co.edu.unbosque.software_electroadonai.model.Usuario;
+import co.edu.unbosque.software_electroadonai.services.EmpleadoDAO;
 import co.edu.unbosque.software_electroadonai.services.UsuarioDAO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
-@RequestMapping("/users")
+@RequestMapping("/usuarios")
 public class UsuarioController {
-
     @Autowired
     private UsuarioDAO usuarioDAO;
     @Autowired
-    private AutoridadesDAO autoridadesDAO;
-    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+    private EmpleadoDAO empleadoDAO;
 
     @GetMapping("/")
+
     public String inicio() {
         return "main";
     }
 
     @GetMapping("/registro")
     public String formularioRegistro(Model model) {
-        model.addAttribute(new Users());
-        model.addAttribute(new Authorities());
+        List<Empleado> empleados = empleadoDAO.getAllEmpleados();
+        model.addAttribute("empleados", empleados);
+        model.addAttribute("usuario", new Usuario());
         return "usuario-form";
     }
 
     @PostMapping("/crear")
-    public String crearEmpleado(@ModelAttribute Users user, @ModelAttribute Authorities authorities) {
-
-        user.setPassword(encoder.encode(user.getPassword()));
-        user.setEnabled(true);
-        usuarioDAO.saveOrUpdate(user);
-        authorities.setUsername(user.getUsername());
-        autoridadesDAO.saveOrUpdate(authorities);
-        return "redirect:/users/listar";
-
+    public String crearEmpleado(@ModelAttribute Usuario usuario) {
+        usuarioDAO.saveOrUpdate(usuario);
+        return "redirect:/usuarios/listar";
     }
-
     @GetMapping("/listar")
     public String listarUsuarios(Model model) {
-        List<Users> users = usuarioDAO.getAllUsuarios();
-        model.addAttribute("users", users);
+        List<Usuario> usuarios = usuarioDAO.getAllUsuarios();
+        model.addAttribute("usuarios", usuarios);
         return "lista-usuarios";
+    }
+    @PostMapping("/editar")
+    public String editarUsuario(@ModelAttribute Usuario nuevoUsuario) {
+        Optional<Usuario> usuarioExistente = usuarioDAO.getUsuarioById(nuevoUsuario.getID_USUARIO());
+        if (usuarioExistente.isPresent()) {
+            Usuario usuario = usuarioExistente.get();
+            usuario.setNOMBRE_USUARIO (nuevoUsuario.getNOMBRE_USUARIO());
+            usuario.setCONTRASENA (nuevoUsuario.getCONTRASENA());
+            usuarioDAO.saveOrUpdate(usuario);
+        }
+        //ResponseEntity.ok("Usuario editado correctamente");
+        return "redirect:/usuarios/listar";
+    }
+
+
+    @DeleteMapping("/eliminar/{id}")
+    public ResponseEntity<String> eliminarUsuario(@PathVariable int id) {
+        Optional<Usuario> usuarioExistente = usuarioDAO.getUsuarioById(id);
+        if (usuarioExistente.isPresent()) {
+            usuarioDAO.deleteUsuario(id);
+            return ResponseEntity.ok("Usuario eliminado correctamente");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuario no encontrado");
+        }
     }
 }
