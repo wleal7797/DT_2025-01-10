bb4df8de847efd6078ef456cd19aed3bcca9af07
wleal797 <leal.wilson19@gmail.com>
2025-04-21 15:56:18 -0500

    Editar y Eliminar listos en todas las vistas

--- Software_ElectroAdonai/src/main/java/co/edu/unbosque/software_electroadonai/controller/DetalleBodegaController.java
+++ Software_ElectroAdonai/src/main/java/co/edu/unbosque/software_electroadonai/controller/DetalleBodegaController.java
@@ -3,21 +3,23 @@ package co.edu.unbosque.software_electroadonai.controller;
 import co.edu.unbosque.software_electroadonai.model.DetalleBodega;
 import co.edu.unbosque.software_electroadonai.services.DetalleBodegaDAO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/detalleBodega")
 public class DetalleBodegaController {
+
     @Autowired
     private DetalleBodegaDAO detalleBodegaDAO;
 
     @GetMapping("/")
-
     public String inicio() {
         return "main";
     }
@@ -27,16 +29,42 @@ public class DetalleBodegaController {
         return "detallesBodega-form";
     }
 
-    @GetMapping("/listarVendedores")
-    public String listarDetallesBodegaVendedores(Model model) {
-        List<DetalleBodega> detalleBodega = detalleBodegaDAO.getAllDetallesBodega();
-        model.addAttribute("detalles", detalleBodega);
-        return "lista-detalleBodegas-vendedor";
-    }
     @GetMapping("/listar")
     public String listarDetallesBodega(Model model) {
         List<DetalleBodega> detalleBodega = detalleBodegaDAO.getAllDetallesBodega();
         model.addAttribute("detalles", detalleBodega);
         return "lista-detalleBodegas";
     }
+
+    @GetMapping("/listarVendedores")
+    public String listarDetallesBodegaVendedores(Model model) {
+        List<DetalleBodega> detalleBodega = detalleBodegaDAO.getAllDetallesBodega();
+        model.addAttribute("detalles", detalleBodega);
+        return "lista-detalleBodegas-vendedor";
+    }
+
+    @PostMapping("/editar")
+    public String editarDetalle(@ModelAttribute DetalleBodega nuevoDetalle) {
+        Optional<DetalleBodega> detalleExistente = detalleBodegaDAO.getDetalleBodegaById(nuevoDetalle.getID_DETALLE_BODEGA());
+
+        if (detalleExistente.isPresent()) {
+            DetalleBodega detalle = detalleExistente.get();
+            detalle.setCNT_PRODUCTO_BODEGA(nuevoDetalle.getCNT_PRODUCTO_BODEGA());
+
+            detalleBodegaDAO.saveOrUpdate(detalle);
+        }
+        return "redirect:/detalleBodega/listar";
+    }
+
+    @DeleteMapping("/eliminar/{id}")
+    public ResponseEntity<String> eliminarDetalle(@PathVariable int id) {
+        Optional<DetalleBodega> detalleExistente = detalleBodegaDAO.getDetalleBodegaById(id);
+        if (detalleExistente.isPresent()) {
+            detalleBodegaDAO.deleteDetalleBodega(id);
+            return ResponseEntity.ok("Detalle eliminado correctamente");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Detalle no encontrado");
+        }
+    }
 }
+
